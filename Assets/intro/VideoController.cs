b13894679550a3722904fbcfe7ab7bc978ddd7d6
Lust <arigaki2013@gmail.com>
2024-05-09 21:16:49 +0200

    Finally fixed that after the Nintendo Splash Screen, the game starts :)

--- Assets/intro/VideoController.cs
+++ Assets/intro/VideoController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
@@ -8,25 +7,15 @@ public class VideoController : MonoBehaviour
     public VideoPlayer videoPlayer;
     public string nextSceneName;
 
-    private bool videoPlayed = false;
-
     void Start()
     {
-        videoPlayer.loopPointReached += VideoFinished;
+        // Subscribe to the video player's loopPointReached event
+        videoPlayer.loopPointReached += OnVideoEnd;
     }
 
-    void VideoFinished(VideoPlayer vp)
+    void OnVideoEnd(VideoPlayer vp)
     {
-        videoPlayed = true;
-    }
-
-    void Update()
-    {
-        // Check if the video has been played completely
-        if (videoPlayed && Mouse.current.leftButton.wasPressedThisFrame) // Using the Input System
-        {
-            // Load the next scene
-            SceneManager.LoadScene(nextSceneName);
-        }
+        // Load the next scene when the video ends
+        SceneManager.LoadScene(nextSceneName);
     }
 }
